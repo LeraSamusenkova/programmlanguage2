e871876e2663ee9023941bb74a25f492c0460fb0
LeraSamusenkova <samusenkova.valeriya@mail.ru>
2023-04-04 12:52:02 +0300

    Пустой проект

---
+++ tetrahedron/Tetrahedron.Tests/Tetrahedron.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TetrahedronTests
+{
+	TEST_CLASS(TetrahedronTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
